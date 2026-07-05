27b2350d994699a9be73c07167b9cc2278f54038
aijorevue <qinnovation013023@163.com>
2026-07-05 22:10:40 +0800

    Finish lab02

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -4,7 +4,18 @@
 #include <string.h>
 #include "lfsr.h"
 
+unsigned get_bit(unsigned x,unsigned n){
+    return ((1<<n)&x)>>n;
+}
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    //我首先想构造一个函数，提取原来的5，3，2，1中的位。
+    unsigned bit0=get_bit(*reg,0);
+    unsigned bit1=get_bit(*reg,2);
+    unsigned bit2=get_bit(*reg,3);
+    unsigned bit3=get_bit(*reg,5);
+    unsigned bit15=bit0^bit1^bit2^bit3;
+    *reg=*reg>>1;
+    *reg=*reg|(bit15<<15);
 }
 

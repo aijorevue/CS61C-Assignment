e1e0e4a906841296ba0fd7136c9b2ea604939dfb
aijorevue <qinnovation013023@163.com>
2026-07-07 14:42:56 +0800

    Finish task 0

--- Project/proj2/src/abs.s
+++ Project/proj2/src/abs.s
@@ -12,8 +12,25 @@ abs:
     # Prologue
 
     # return 0
-    mv a0, zero
+    #mv a0, zero
+    
+    #这是他的初始代码，相当于
+    #int abs(int a0){
+    #    a0=0;
+    #    return a0; 
+    #}
+    #而如果你想测试他：
+    #python3 -m unittest unittests.TestAbs -v
 
+    #
     # Epilogue
 
+    #以下为所给实例代码用来通过test_minus_one
+    #branch if positive
+    bge a0,zero,done
+
+    #invert a if negative
+    sub a0,zero,a0
+
+done:
     ret

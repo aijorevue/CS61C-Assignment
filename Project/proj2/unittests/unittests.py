8d647aae3f2d89947ff5741860b0bd1a5c54dc09
aijorevue <qinnovation013023@163.com>
2026-07-17 14:02:32 +0800

    Finish Proj2-3.2

--- Project/proj2/unittests/unittests.py
+++ Project/proj2/unittests/unittests.py
@@ -123,6 +123,19 @@ class TestDot(TestCase):
         t.check_scalar("a0", 22)
         t.execute()
 
+    def test_length_not_same(self):
+        t=AssemblyTest(self,"dot.s")
+        array0=t.array([1,2,3])
+        array1=t.array([4,5])
+        t.input_array("a0",array0)
+        t.input_array("a1",array1)
+        t.input_scalar("a2",3)
+        t.input_scalar("a3",1)
+        t.input_scalar("a4",2)
+        t.call("dot")
+        t.check_scalar("a0",4)
+        t.execute()
+        
     @classmethod
     def tearDownClass(cls):
         print_coverage("dot.s", verbose=False)
@@ -141,17 +154,21 @@ class TestMatmul(TestCase):
         array_out = t.array([0] * len(result))
 
         # load address of input matrices and set their dimensions
-        raise NotImplementedError("TODO")
-        # TODO
+        t.input_array("a0", array0)
+        t.input_scalar("a1", m0_rows)
+        t.input_scalar("a2", m0_cols)
+        t.input_array("a3", array1)
+        t.input_scalar("a4", m1_rows)
+        t.input_scalar("a5", m1_cols)
+
         # load address of output array
-        # TODO
+        t.input_array("a6", array_out)
 
         # call the matmul function
         t.call("matmul")
 
         # check the content of the output array
-        # TODO
-
+        t.check_array(array_out, result)
         # generate the assembly file and run it through venus, we expect the simulation to exit with code `code`
         t.execute(code=code)
 
